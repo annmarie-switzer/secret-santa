cc2ad9ed52cc354601562da0ae2a94a7e586223f
Annmarie Switzer <aargiros@gmail.com>
2022-09-05 22:23:11 -0400

    set up global CSS

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { HelloWorld } from './src/HelloWorld';
 
-export default function App() {
+const App = () => {
     return (
         <View style={styles.container}>
-            <Text>Open up App.tsx to start working on your app!</Text>
+            <HelloWorld />
             <StatusBar style="auto" />
         </View>
     );
@@ -15,6 +16,8 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
     }
 });
+
+export default App;
